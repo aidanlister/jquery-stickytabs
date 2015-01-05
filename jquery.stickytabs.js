03975aa53d39fc9f1cd19f2a7f26e0f674eca1e0
Aidan Lister <aidan@aidanlister.com>
2015-01-05 16:12:11 +0530

    Add main script and a basic page to test on.

---
+++ jquery.stickytabs.js
@@ -0,0 +1,32 @@
+/**
+ * jQuery Plugin: Sticky Tabs
+ *
+ * @author Aidan Lister <aidan@php.net>
+ * @version 1.0.1
+ */
+(function ( $ ) {
+    $.fn.stickyTabs = function() {
+        context = this
+
+        // Show the tab corresponding with the hash in the URL, or the first tab.
+        var showTabFromHash = function() {
+          var hash = window.location.hash;
+          var selector = hash ? 'a[href="' + hash + '"]' : 'li.active > a';
+          $(selector, context).tab('show');
+        }
+
+        // Set the correct tab when the page loads
+        showTabFromHash(context)
+
+        // Set the correct tab when a user uses their back/forward button
+        window.addEventListener('hashchange', showTabFromHash, false);
+
+        // Change the URL when tabs are clicked
+        $('a', context).on('click', function(e) {
+          history.pushState(null, null, this.href);
+        });
+
+        return this;
+    };
+}( jQuery ));
+
