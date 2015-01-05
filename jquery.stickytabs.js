ba06d88fecfb281eb6dcbe177a8104994206f8d6
Aidan Lister <aidan@aidanlister.com>
2015-01-05 17:07:08 +0530

    Implements the pullrequest in https://github.com/aidanlister/code/pull/2 which allows changing the URL before sending to pushState.

--- jquery.stickytabs.js
+++ jquery.stickytabs.js
@@ -5,8 +5,12 @@
  * @version 1.0.1
  */
 (function ( $ ) {
-    $.fn.stickyTabs = function() {
-        context = this
+    $.fn.stickyTabs = function( options ) {
+        var context = this
+
+        var settings = $.extend({
+            getHashCallback: function(hash, btn) { return hash }
+        }, options );
 
         // Show the tab corresponding with the hash in the URL, or the first tab.
         var showTabFromHash = function() {
@@ -21,12 +25,13 @@
         // Set the correct tab when a user uses their back/forward button
         window.addEventListener('hashchange', showTabFromHash, false);
 
-        // Change the URL when tabs are clicked
+        // Change the URL when tabs are clicked using
+        //   window.location.hash which sets the state without needing pushState.
         $('a', context).on('click', function(e) {
-          history.pushState(null, null, this.href);
+          var hash = this.href.split('#')[1];
+          window.location.hash = settings.getHashCallback(hash, this)
         });
 
         return this;
     };
 }( jQuery ));
-
