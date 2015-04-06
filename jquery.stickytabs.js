da2c59300144c9559833787780beadf5383be596
Curtis Weeks <curtis@jabbtech.com>
2015-04-06 16:18:02 -0500

    Added test for backToTop setting

--- jquery.stickytabs.js
+++ jquery.stickytabs.js
@@ -20,11 +20,7 @@
           var hash = settings.selectorAttribute == "href" ? window.location.hash : window.location.hash.substring(1);
           var selector = hash ? 'a[' + settings.selectorAttribute +'="' + hash + '"]' : settings.initialTab;
           $(selector, context).tab('show');
-          if (settings.backToTop === true) {
-            setTimeout (function () {
-              window.scrollTo(0, 0);
-            }, 1);
-          }
+          setTimeout(backToTop, 1);
         }
 
         // We use pushState if it's available so the page won't jump, otherwise a shim.
@@ -40,6 +36,12 @@
           }
         }
 
+        var backToTop = function() {
+          if (settings.backToTop === true) {
+            window.scrollTo(0, 0);
+          }
+        }
+
         // Set the correct tab when the page loads
         showTabFromHash();
 
@@ -51,6 +53,7 @@
           var hash = this.href.split('#')[1];
           var adjustedhash = settings.getHashCallback(hash, this);
           changeHash(adjustedhash);
+          setTimeout(backToTop, 1);
         });
 
         return this;
