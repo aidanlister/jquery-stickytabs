e9320e15f5cc4fa16154ac8e03dc987d1a5b955e
Curtis Weeks <curtis@jabbtech.com>
2015-05-04 13:02:18 -0500

    Lint

--- jquery.stickytabs.js
+++ jquery.stickytabs.js
@@ -1,51 +1,61 @@
 /**
- * jQuery Plugin: Sticky Tabs
- *
- * @author Aidan Lister <aidan@php.net>
- * @version 1.2.0
- */
-(function ( $ ) {
-    $.fn.stickyTabs = function( options ) {
-        var context = this
+* jQuery Plugin: Sticky Tabs
+*
+* @author Aidan Lister <aidan@php.net>
+* @version 1.2.0
+*/
 
-        var settings = $.extend({
-            getHashCallback: function(hash, btn) { return hash },
+(function ($) {
+
+    'use strict';
+
+    $.fn.stickyTabs = function (options) {
+
+        var context, settings, showTabFromHash, changeHash, backToTop;
+
+        context = this;
+        settings = $.extend({
+            getHashCallback: function (hash, btn) {
+                return hash;
+            },
             tabPersistence: true,
             selectorAttribute: "href",
             backToTop: false,
             initialTab: $('li.active > a', context)
-        }, options );
+        }, options);
 
         // Show the tab corresponding with the hash in the URL, or the first tab.
-        var showTabFromHash = function() {
-          var hash = settings.selectorAttribute == "href" ? window.location.hash : window.location.hash.substring(1);
-          var selector = hash ? 'a[' + settings.selectorAttribute +'="' + hash + '"]' : settings.initialTab;
-          $(selector, context).tab('show');
-          setTimeout(backToTop, 1);
-        }
+        showTabFromHash = function () {
+            var hash, selector;
+            hash = (settings.selectorAttribute === "href") ? window.location.hash : window.location.hash.substring(1);
+            selector = hash ? 'a[' + settings.selectorAttribute + '="' + hash + '"]' : settings.initialTab;
+            $(selector, context).tab('show');
+            setTimeout(backToTop, 1);
+        };
 
         // We use pushState if it's available so the page won't jump, otherwise a shim.
-        var changeHash = function(hash) {
-          var url = window.location.pathname + window.location.search;
-          if (history && history.pushState && settings.tabPersistence === true) {
-            url += '#' + hash;
-            history.pushState(null, null, url);
-          } else if (history && history.replaceState) {
-            history.replaceState(null, null, url);
-          } else {
-              scrollV = document.body.scrollTop;
-              scrollH = document.body.scrollLeft;
-              window.location.hash = hash;
-              document.body.scrollTop = scrollV;
-              document.body.scrollLeft = scrollH;
+        changeHash = function (hash) {
+            var url, sV, sH;
+            url = window.location.pathname + window.location.search;
+            if (history && history.pushState && settings.tabPersistence === true) {
+                url += '#' + hash;
+                history.pushState(null, null, url);
+            } else if (history && history.replaceState) {
+                history.replaceState(null, null, url);
+            } else {
+                sV = document.body.scrollTop;
+                sH = document.body.scrollLeft;
+                window.location.hash = hash;
+                document.body.scrollTop = sV;
+                document.body.scrollLeft = sH;
             }
-          }
+        };
 
-        var backToTop = function() {
-          if (settings.backToTop === true) {
-            window.scrollTo(0, 0);
-          }
-        }
+        backToTop = function () {
+            if (settings.backToTop === true) {
+                window.scrollTo(0, 0);
+            }
+        };
 
         // Set the correct tab when the page loads
         showTabFromHash();
@@ -54,13 +64,15 @@
         $(window).on('hashchange', showTabFromHash);
 
         // Change the URL when tabs are clicked
-        $('a', context).on('click', function(e) {
-          var hash = this.href.split('#')[1];
-          var adjustedhash = settings.getHashCallback(hash, this);
-          changeHash(adjustedhash);
-          setTimeout(backToTop, 1);
+        $('a', context).on('click', function () {
+            var hash, adjustedhash;
+            hash = this.href.split('#')[1];
+            adjustedhash = settings.getHashCallback(hash, this);
+            changeHash(adjustedhash);
+            setTimeout(backToTop, 1);
         });
 
         return this;
     };
-}( jQuery ));
+
+}(jQuery));
