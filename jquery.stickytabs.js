25c02080245890ad43221dfefe24197e2df8a6fa
Menno Holtkamp <menno.holtkamp@shopforce.nl>
2015-09-22 01:08:25 +0200

    minor improvements and formatting

--- jquery.stickytabs.js
+++ jquery.stickytabs.js
@@ -4,43 +4,45 @@
  * @author Aidan Lister <aidan@php.net>
  * @version 1.2.0
  */
-(function ( $ ) {
-    $.fn.stickyTabs = function( options ) {
-        var context = this
+(function ($) {
+    $.fn.stickyTabs = function (options) {
+        var context = this;
 
         var settings = $.extend({
-            getHashCallback: function(hash, btn) { return hash },
+            getHashCallback: function (hash, btn) {
+                return hash
+            },
             selectorAttribute: "href",
             backToTop: false,
             initialTab: $('li.active > a', context)
-        }, options );
+        }, options);
 
         // Show the tab corresponding with the hash in the URL, or the first tab.
-        var showTabFromHash = function() {
-          var hash = settings.selectorAttribute == "href" ? window.location.hash : window.location.hash.substring(1);
-          var selector = hash ? 'a[' + settings.selectorAttribute +'="' + hash + '"]' : settings.initialTab;
-          $(selector, context).tab('show');
-          setTimeout(backToTop, 1);
-        }
+        var showTabFromHash = function () {
+            var hash = settings.selectorAttribute == "href" ? window.location.hash : window.location.hash.substring(1);
+            var selector = hash ? 'a[' + settings.selectorAttribute + '="' + hash + '"]' : settings.initialTab;
+            $(selector, context).tab('show');
+            setTimeout(backToTop, 1);
+        };
 
-        // We use pushState if it's available so the page won't jump, otherwise a shim.
-        var changeHash = function(hash) {
-          if (history && history.pushState) {
-            history.pushState(null, null, window.location.pathname + window.location.search + '#' + hash);
-          } else {
-            scrollV = document.body.scrollTop;
-            scrollH = document.body.scrollLeft;
-            window.location.hash = hash;
-            document.body.scrollTop = scrollV;
-            document.body.scrollLeft = scrollH;
-          }
-        }
+        // Use pushState if it is available so the page will not jump, otherwise a shim.
+        var changeHash = function (hash) {
+            if (history && history.pushState) {
+                history.pushState(null, null, window.location.pathname + window.location.search + '#' + hash);
+            } else {
+                var scrollVertical = document.body.scrollTop;
+                var scrollHorizontal = document.body.scrollLeft;
+                window.location.hash = hash;
+                document.body.scrollTop = scrollVertical;
+                document.body.scrollLeft = scrollHorizontal;
+            }
+        };
 
-        var backToTop = function() {
-          if (settings.backToTop === true) {
-            window.scrollTo(0, 0);
-          }
-        }
+        var backToTop = function () {
+            if (settings.backToTop === true) {
+                window.scrollTo(0, 0);
+            }
+        };
 
         // Set the correct tab when the page loads
         showTabFromHash();
@@ -49,13 +51,13 @@
         $(window).on('hashchange', showTabFromHash);
 
         // Change the URL when tabs are clicked
-        $('a', context).on('click', function(e) {
-          var hash = this.href.split('#')[1];
-          var adjustedhash = settings.getHashCallback(hash, this);
-          changeHash(adjustedhash);
-          setTimeout(backToTop, 1);
+        $('a', context).on('click', function (e) {
+            var hash = this.href.split('#')[1];
+            var adjustedHash = settings.getHashCallback(hash, this);
+            changeHash(adjustedHash);
+            setTimeout(backToTop, 1);
         });
 
         return this;
     };
-}( jQuery ));
+}(jQuery));
